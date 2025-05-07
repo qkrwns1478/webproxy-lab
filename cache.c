b40a247e72e509b6fd714f9da5edbace82df3256
qkrwns1478 <qkrwns1478@gmail.com>
2025-05-07 21:52:46 +0900

    #8 caching with LRU

--- cache.c
+++ cache.c
@@ -3,7 +3,12 @@
 
 cache_entry_t cache[CACHE_ENTRIES];
 pthread_mutex_t cache_manager_lock;
-int cache_index = 0;
+
+/* LRU Replacement Policy 
+ * 캐시 엔트리에 timestamp를 선언해 캐시가 저장된 or 참조된 시간을 업데이트한다.
+ * 캐시 테이블에 새로운 캐시를 삽입해야 할 때
+ * 가장 timestamp가 오래된 캐시 엔트리를 찾아서 새로운 캐시 데이터로 대체한다.
+ */
 
 // Initialize cache
 void cache_init() {
@@ -11,6 +16,7 @@ void cache_init() {
         cache[i].valid = 0;
         cache[i].object_size = 0;
         cache[i].read_count = 0;
+        cache[i].timestamp = time(NULL);
         pthread_rwlock_init(&cache[i].lock, NULL);
     }
     pthread_mutex_init(&cache_manager_lock, NULL);
@@ -27,6 +33,7 @@ int read_cache(char *uri, int connfd) {
             Rio_writen(connfd, cache[i].object, cache[i].object_size);
             hit = 1;
             cache[i].read_count++;
+            cache[i].timestamp = time(NULL); // 참조된 캐시 엔트리 타임스탬프 최신화
         }
 
         pthread_rwlock_unlock(&cache[i].lock);
@@ -38,6 +45,21 @@ int read_cache(char *uri, int connfd) {
 
 // Write response data from server on cache
 void write_cache(char *uri, char *object, int object_size) {
+    int cache_index = 0;
+    int oldest_time = time(NULL);
+
+    // cache_index를 timestamp가 가장 작은 것으로 선택
+    for (int i = 0; i < CACHE_ENTRIES; i++) {
+        pthread_rwlock_rdlock(&cache[i].lock);
+
+        if (cache[i].timestamp <= oldest_time) {
+            oldest_time = cache[i].timestamp;
+            cache_index = i;
+        }
+
+        pthread_rwlock_unlock(&cache[i].lock);
+    }
+
     pthread_rwlock_wrlock(&cache[cache_index].lock);
 
     // 이전 캐시 항목 정보 무효화
@@ -51,11 +73,9 @@ void write_cache(char *uri, char *object, int object_size) {
     memcpy(cache[cache_index].object, object, object_size);
     cache[cache_index].object_size = object_size;
     cache[cache_index].valid = 1;
+    cache[cache_index].timestamp = time(NULL);
 
     pthread_rwlock_unlock(&cache[cache_index].lock);
-
-    // 다음 캐시 항목으로 인덱스 이동
-    cache_index = (cache_index + 1) % CACHE_ENTRIES;
 }
 
 void cache_destroy() {
